39918bb71a0a2e722351ae54fa52a86ecd902b47
kedistkid <kedistkid723@gmail.com>
2024-03-29 09:13:04 +0300

    add sample data from the dataset

--- biocypher_metta/adapters/enhancer_ccre_adapter.py
+++ biocypher_metta/adapters/enhancer_ccre_adapter.py
@@ -1,17 +1,11 @@
 from biocypher_metta.adapters import Adapter
 
-# Example data from encodeCcreCombined.bed file:
-# chr1	3119617	3119911	EM10E0431203	303	.	3119617	3119911	255,205,0	dELS	dELS	3.03931270232	enhD	E0431203	EM10E0431203 distal enhancer-like signature
-# chr1	3119914	3120120	EM10E0431204	264	.	3119914	3120120	255,205,0	dELS	dELS	2.64144130291	enhD	E0431204	EM10E0431204 distal enhancer-like signature
-# chr1	3120346	3120662	EM10E0431205	263	.	3120346	3120662	255,205,0	dELS	dELS	2.63858128228	enhD	E0431205	EM10E0431205 distal enhancer-like signature
-# chr1	3292622	3292971	EM10E0431207	249	.	3292622	3292971	255,205,0	dELS	dELS	2.495384694	enhD	E0431207	EM10E0431207 distal enhancer-like signature
-
-
 class EnhancerCCREAdapter(Adapter):
     def __init__(self, filepath, label):
         self.filepath = filepath
         self.label = label
         self.source = "EnhancerCCRE"
+        self.source_url = "URL to dataset source"
         super(EnhancerCCREAdapter, self).__init__()
 
     def get_nodes(self):

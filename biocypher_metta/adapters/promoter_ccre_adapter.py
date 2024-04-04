286c29e3e12d4a6b32d4f2f24075d4e7764cf680
kedistkid <kedistkid723@gmail.com>
2024-04-04 14:45:02 +0300

    change dataset sample example and source url

--- biocypher_metta/adapters/promoter_ccre_adapter.py
+++ biocypher_metta/adapters/promoter_ccre_adapter.py
@@ -2,10 +2,11 @@ from biocypher_metta.adapters import Adapter
 import gzip
 
 # Example data from the dataset encodeCcreCombined.bed:
-# chr1	3119617	3119911	EM10E0431203	303	.	3119617	3119911	255,205,0	dELS	dELS	3.03931270232	enhD	E0431203	EM10E0431203 distal enhancer-like signature
-# chr1	3119914	3120120	EM10E0431204	264	.	3119914	3120120	255,205,0	dELS	dELS	2.64144130291	enhD	E0431204	EM10E0431204 distal enhancer-like signature
 # chr1	3120346	3120662	EM10E0431205	263	.	3120346	3120662	255,205,0	dELS	dELS	2.63858128228	enhD	E0431205	EM10E0431205 distal enhancer-like signature
-# chr1	3292622	3292971	EM10E0431207	249	.	3292622	3292971	255,205,0	dELS	dELS	2.495384694	enhD	E0431207	EM10E0431207 distal enhancer-like signature
+# chr1	3772420	3772769	EH38E1313009	595	.	3772420	3772769	255,0,0	PLS	PLS	5.957252591	prom	E1313009	EH38E1313009 promoter-like signature
+# chr1	3772821	3773112	EH38E1313010	368	.	3772821	3773112	255,0,0	PLS,CTCF-bound	PLS	3.681354874	prom	E1313010	EH38E1313010 promoter-like signature
+# chr1	3796383	3796733	EH38E1313041	536	.	3796383	3796733	255,0,0	PLS,CTCF-bound	PLS	5.367484968	prom	E1313041	EH38E1313041 promoter-like signature
+
 
 class PromoterCCREAdapter(Adapter):
     def __init__(self, filepath, write_properties, add_provenance, label="promoter", type="promoter"):
@@ -13,8 +14,7 @@ class PromoterCCREAdapter(Adapter):
         self.label = label
         self.type = type  
         self.source = "PromoterCCRE"
-        self.source_url = "https://genome.ucsc.edu/cgi-bin/hgTrackUi?db=mm10&g=encodeCcreCombined"
-
+        self.source_url = "https://genome.ucsc.edu/cgi-bin/hgTables?db=hg38&hgta_group=regulation&hgta_track=encodeCcreCombined&hgta_table=encodeCcreCombined&hgta_doSchema=describe+table+schema"
         super(PromoterCCREAdapter, self).__init__(write_properties, add_provenance)
 
     def get_nodes(self):

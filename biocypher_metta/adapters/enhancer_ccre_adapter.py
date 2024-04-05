08c37008aef0dd6a0d975258930bc7424765d5b2
kedistkid <kedistkid723@gmail.com>
2024-04-05 14:08:33 +0300

    add header to sample dataset

--- biocypher_metta/adapters/enhancer_ccre_adapter.py
+++ biocypher_metta/adapters/enhancer_ccre_adapter.py
@@ -2,11 +2,12 @@ import gzip
 from biocypher_metta.adapters import Adapter
 
 # Example data from the dataset encodeCcreCombined.bed:
-# chr1	3119617	3119911	EM10E0431203	303	.	3119617	3119911	255,205,0	dELS	dELS	3.03931270232	enhD	E0431203	EM10E0431203 distal enhancer-like signature
-# chr1	3119914	3120120	EM10E0431204	264	.	3119914	3120120	255,205,0	dELS	dELS	2.64144130291	enhD	E0431204	EM10E0431204 distal enhancer-like signature
-# chr1	3120346	3120662	EM10E0431205	263	.	3120346	3120662	255,205,0	dELS	dELS	2.63858128228	enhD	E0431205	EM10E0431205 distal enhancer-like signature
-# chr1	3292622	3292971	EM10E0431207	249	.	3292622	3292971	255,205,0	dELS	dELS	2.495384694	enhD	E0431207	EM10E0431207 distal enhancer-like signature
-
+# chrom start end gene score strand chromStart chromEnd itemRgb signatures category signalValue type accessionId description
+# chr1	181251	181601	EH38E1310153	488	.	181251	181601	255,167,0	pELS,CTCF-bound	pELS	4.88403259983	enhP	E1310153	EH38E1310153 proximal enhancer-like signature
+# chr1	190865	191071	EH38E1310154	179	.	190865	191071	255,205,0	dELS,CTCF-bound	dELS	1.79282201562	enhD	E1310154	EH38E1310154 distal enhancer-like signature
+# chr1	778562	778912	EH38E1310158	759	.	778562	778912	255,0,0	PLS,CTCF-bound	PLS	7.59852335807	prom	E1310158	EH38E1310158 promoter-like signature
+# chr1	779086	779355	EH38E1310159	304	.	779086	779355	255,0,0	PLS,CTCF-bound	PLS	3.04663905401	prom	E1310159	EH38E1310159 promoter-like signature
+# chr1	779727	780060	EH38E1310160	281	.	779727	780060	255,167,0	pELS,CTCF-bound	pELS	2.81743397307	enhP	E1310160	EH38E1310160 proximal enhancer-like signature
 
 class EnhancerCCREAdapter(Adapter):
     def __init__(self, filepath,write_properties, add_provenance, label='enhancer', type='enhancer'):
@@ -26,7 +27,6 @@ class EnhancerCCREAdapter(Adapter):
                     start = int(fields[1]) + 1
                     end = int(fields[2])  + 1
 
-                    # Handle missing or invalid fields
                     try:
                         score = float(fields[4])
                         gene_data = fields[3]
@@ -53,12 +53,11 @@ class EnhancerCCREAdapter(Adapter):
                 if line.startswith("chr"):
                     fields = line.strip().split("\t")
                     chrom = fields[0]
-                    start = int(fields[1])  
-                    end = int(fields[2])  
+                    start = int(fields[1]) + 1 
+                    end = int(fields[2])  + 1
                     gene_data = fields[3]
                     region_type = fields[10]  
 
-                    # Handle missing or invalid fields
                     if gene_data == ".":
                         continue
 

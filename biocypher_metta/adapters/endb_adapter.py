167737b1a2a2abca957813ad2fc13bacb9b54991
Dawit Melka <davebadeg@gmail.com>
2024-04-17 13:50:52 +0300

    Update check genomic location

--- biocypher_metta/adapters/endb_adapter.py
+++ biocypher_metta/adapters/endb_adapter.py
@@ -3,6 +3,7 @@ import pickle
 
 from biocypher_metta.adapters import Adapter
 from liftover import get_lifter
+from biocypher_metta.adapters.helpers import check_genomic_location
 # Example ENdb csv input file:
 # Enhancer_id	Pubmed	Enhancer_symbol	Reference_genome	Chromosome	Start_position	End_position	Species	Biosample_name	Experiment_class	Enhancer_experiment	Enhancer_tar_ex_De	Enhancer_type	Target_gene	target_gene_strong_experiment	target_gene_weak_experiment	target_gene_experiment_description	target_gene_other_name	Disease	DO	mesh	Enhancer_function	Enhancer_function_experiment	En_function_ex_de	TF_name	TF_other_name	Experiment	TF_experiment_de	SNP_id	SNP_position	SNP_experiment	Distance_from_TSS	Distance_from_TSS_AB	enhancer_MID	TSS	n_Distance_from_TSS
 # E_01_001	28652246	--	hg19	chr6	86160197	86176777	Human	Melanoma	Low+High throughput	"ChIP-seq,ChIP-qPCR"	A ChIP-qPCR approach revealed prominent binding of c-Jun at the AP-1 sites #1-2 and #5-6 (Fig. 3E) coinciding with enhancer regions (H3K27Ac) identified by the ENCODE ChIP-seq project (Fig. 3D).	Enhancer	NT5E	CRISPR/Cas9	"qPCR,ChIP-qPCR"	"Next, we devised a CRISPR/Cas9 approach to clarify the functional relevance of the individual c-Jun/AP-1 binding sites.c-Jun/AP-1 induces CD73 expression via binding to an intronic enhancer.Strong enrichment of AP-1 site mutagenesis frequencies in the CD73 low subfractions (52% versus 4% in CD73 lowest 10% versus highest 10%) was observed only in the cell population transfected with the sgRNA against the c-Jun/AP-1 site #5 (Fig. 3G-H) being consistent with reduced CD73 surface expression (Fig. S4A)."	"CALJA,CD73,E5NT,NT,NT5,NTE,eN,eNT"	Melanoma	DOID:1909	D008545	The intronic Enhancer of the CD73 gene controls its c-Jun/AP-1-dependent transcriptional activation downstream of mitogenic MAPK and inflammatory cytokine signaling. 	"ChIP-qPCR,CRISPR/Cas9"	"Taken together, we identified a critical c-Jun/AP-1 binding site in an intronic enhancer of the CD73 gene that controls its c-Jun/AP-1-dependent transcriptional activation downstream of mitogenic MAPK and inflammatory cytokine signaling."	JUN	"AP-1,AP1,c-Jun,cJUN,p39"	ChIP-qPCR	We looked for potential c-Jun/AP-1�Cbinding sites in the CD73 gene using ENCODE ChIP-Seq data.A ChIP-qPCR approach revealed prominent binding of c-Jun at the AP-1 sites #1�C2 and #5�C6 coinciding with Enhancer regions (H3K27Ac)identified by the ENCODE ChIP-seq project.	--	--	--	Intron				9186
@@ -14,12 +15,17 @@ class ENdbAdapter(Adapter):
     ALLOWED_LABELS = ['enhancer_regulates']
     INDEX = {'enhancer_id': 0, 'pubmed': 1,  'chr': 4, 'coord_start': 5, 'coord_end': 6, 'species' : 7, 'enhancer_type': 12, 'gene_id': 13, 'strong_experiment': 14, 'disease': 18, 'tf_name': 24, 'snp_id': 28}
     
-    def __init__(self, filepath, hgnc_to_ensembl_map, write_properties, add_provenance, type='enhancer', label='enhancer', delimiter='\t'):
+    def __init__(self, filepath, hgnc_to_ensembl_map, write_properties, add_provenance,
+                 type='enhancer', label='enhancer', delimiter='\t',
+                 chr=None, start=None, end=None):
         self.filepath = filepath
         self.hgnc_to_ensembl_map = pickle.load(open(hgnc_to_ensembl_map, 'rb'))
         self.type = type
         self.label = label 
         self.delimiter = delimiter
+        self.chr = chr
+        self.start = start
+        self.end = end
         self.genome_reference_converter = get_lifter('hg19', 'hg38')
 
         self.source = 'ENdb'
@@ -93,21 +99,22 @@ class ENdbAdapter(Adapter):
                 snp_id = data[ENdbAdapter.INDEX['snp_id']]
                 # disease = data[ENdbAdapter.INDEX['disease']]
                 
-                props = {}
-                if self.write_properties:
-                    props['chr'] = chr
-                    props['start'] = start_position
-                    props['end'] = end_position
-                    if tf_name != '--':
-                        props['tf_name'] = self.parse_info_metadata(tf_name)
-                    if snp_id != '--':
-                        props['snp_id'] = self.parse_info_metadata(snp_id)
+                if check_genomic_location(self.chr, self.start, self.end, chr, start_position, end_position):
+                    props = {}
+                    if self.write_properties:
+                        props['chr'] = chr
+                        props['start'] = start_position
+                        props['end'] = end_position
+                        if tf_name != '--':
+                            props['tf_name'] = self.parse_info_metadata(tf_name)
+                        if snp_id != '--':
+                            props['snp_id'] = self.parse_info_metadata(snp_id)
 
-                    if self.add_provenance:
-                        props['source'] = self.source
-                        props['source_url'] = self.source_url
+                        if self.add_provenance:
+                            props['source'] = self.source
+                            props['source_url'] = self.source_url
 
-                yield enhancer_id, self.label, props
+                    yield enhancer_id, self.label, props
 
     def get_edges(self):
         self.temp = []
@@ -151,16 +158,17 @@ class ENdbAdapter(Adapter):
                 target_genes_list = self.handle_target_genes(target_genes.upper())
                 experiment = data[ENdbAdapter.INDEX['strong_experiment']]
                 
-                props = {}
-                if self.write_properties:
-                    props['evidence'] = pubmed
-                    if experiment != '--':
-                        props['experiment'] = self.parse_info_metadata(experiment)
+                if check_genomic_location(self.chr, self.start, self.end, chr, start_position, end_position):
+                    props = {}
+                    if self.write_properties:
+                        props['evidence'] = pubmed
+                        if experiment != '--':
+                            props['experiment'] = self.parse_info_metadata(experiment)
 
-                    if self.add_provenance:
-                        props['source'] = self.source
-                        props['source_url'] = self.source_url
+                        if self.add_provenance:
+                            props['source'] = self.source
+                            props['source_url'] = self.source_url
 
-                for gene in target_genes_list:
-                    yield enhancer_id, gene.strip(), self.label, props
+                    for gene in target_genes_list:
+                        yield enhancer_id, gene.strip(), self.label, props
 

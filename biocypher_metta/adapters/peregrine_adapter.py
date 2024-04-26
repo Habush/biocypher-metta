f919cce9b3c1433e872aa25bfd050574d068d1cc
Dawit Melka <davebadeg@gmail.com>
2024-04-26 12:11:54 +0300

    Add tissue as a field in getEdges props

--- biocypher_metta/adapters/peregrine_adapter.py
+++ biocypher_metta/adapters/peregrine_adapter.py
@@ -3,6 +3,7 @@ import gzip
 import pickle
 
 from biocypher_metta.adapters import Adapter
+from biocypher_metta.adapters.helpers import build_regulatory_region_id, check_genomic_location
 # Example PEREGRINE input files:
 
 # PEREGRINEenhancershg38
@@ -10,10 +11,12 @@ from biocypher_metta.adapters import Adapter
 # chr1	99638888	99639130	2
 # chr1	99673438	99673767	5
 
-# PREGRINE scores
-# enhancer	gene	HepG2score	HepG2Zscore	CDF_HepG2	HCT116score	HCT116Zscore	CDF_HCT116	K562score	K562Zscore	CDF_K562	MCF7score	MCF7Zscore	CDF_MCF7
-# 1	HUMAN|HGNC=15846|UniProtKB=Q9NP74	0.14231604149762	0.364411398538007	0.799181060202044	0.1576516981884	-0.337757438945174	0.873174506724474	0.0344599217044222	-0.337757438945174	0.0254839982033111	0.17554256461814	0.733623642220683	0.906677165599342
-# 1	HUMAN|HGNC=321|UniProtKB=P35573	0.0430095105578992	-0.563353003049424	0.241575024295348	0.053969581878424	-0.242133777354291	0.369753681325124	0.0462475325608546	-0.242133777354291	0.504521945621637	0.133478871732277	0.259622696593164	0.71453419341604
+# enhancer_gene_link_18.tsv
+# enhancer	gene	linkID	assay	tissue	p-value	eQTL_SNP_ID	score	Zscore	CDFscore
+# 1	HUMAN|HGNC=15846|UniProtKB=Q9NP74	1	3	64		
+# 1	HUMAN|HGNC=15846|UniProtKB=Q9NP74	1	3	65		
+# 1	HUMAN|HGNC=15846|UniProtKB=Q9NP74	1	3	66		
+# 1	HUMAN|HGNC=15846|UniProtKB=Q9NP74	1	3	67		
 
 # PEREGRINE datasources
 # 1	FANTOM
@@ -27,17 +30,25 @@ class PEREGRINEAdapter(Adapter):
     ALLOWED_TYPES = ['enhancer', 'enhancer to gene association']
     ALLOWED_LABELS = ['enhancer', 'enhancer_gene']
     ALLOWED_KEYS = []
-    INDEX = {'enhancer': 0,	'gene': 1}
+    INDEX = {'enhancer': 0, 'gene': 1, 'tissue': 4, 'score': 7, 'chr': 0, 'start': 1, 'end': 2, 'id': 3}
 
-    def __init__(self, enhancers_file, enhancer_gene_link_scores, source_file, hgnc_ensembl_map, hgnc_symbol_map, write_properties, add_provenance, type='enhancer', label='enhancer', delimiter='\t'):
+    def __init__(self, enhancers_file, enhancer_gene_link, 
+                 source_file, hgnc_ensembl_map, 
+                 tissue_ontology_map, write_properties, add_provenance, 
+                 type='enhancer', label='enhancer', delimiter='\t',
+                 chr=None, start=None, end=None):
+        
         self.enhancers_file = enhancers_file
-        self.enhancer_gene_link_scores = enhancer_gene_link_scores
+        self.enhancer_gene_link = enhancer_gene_link
         self.source_file = source_file
         self.hgnc_ensembl_map = pickle.load(open(hgnc_ensembl_map, 'rb'))
-        self.hgnc_symbol_map = pickle.load(open(hgnc_symbol_map, 'rb'))
+        self.tissue_ontology_map = pickle.load(open(tissue_ontology_map, 'rb'))
         self.type = type
         self.label = label
         self.delimiter = delimiter
+        self.chr = chr
+        self.start = start
+        self.end = end
 
         self.source = 'PEREGRINE'
         self.version = ''
@@ -48,13 +59,8 @@ class PEREGRINEAdapter(Adapter):
     def handel_gene(self, gene):
         gene = gene.split('|')[1]
         gene = ':'.join(gene.split('='))
-        if gene in self.hgnc_ensembl_map:
-            gene = self.hgnc_ensembl_map[gene]
-        else:
-            gene = self.hgnc_symbol_map.get(gene, gene)
-
         return gene
-
+        
     def get_nodes(self):
         enhancer_info = {}
         with gzip.open(self.enhancers_file, 'rt') as f:
@@ -63,8 +69,8 @@ class PEREGRINEAdapter(Adapter):
                 chr, start, end, enhancer_id = line
                 enhancer_info[enhancer_id] = {
                     'chr': chr,
-                    'start': start,
-                    'end': end
+                    'start': int(start),
+                    'end': int(end),
                 }
 
         source_map = {}
@@ -74,38 +80,67 @@ class PEREGRINEAdapter(Adapter):
                 source_map[enhancer_id] = source
 
         for enhancer_id, info in enhancer_info.items():
-            data_source = source_map.get(enhancer_id, 'NA')
-
+            data_source = source_map[enhancer_id]
+            chr = info['chr']
+            start = info['start']
+            end = info['end']
+            enhancer_region_id = build_regulatory_region_id(chr, start, end)
             props = {}
-
-            if self.write_properties:
-                props['chr'] = info['chr']
-                props['start'] = info['start']
-                props['end'] = info['end']
-                props['data_source'] = data_source
-
-                if self.add_provenance:
-                    props['source'] = self.source
-                    props['source_url'] = self.source_url
-            
-            yield enhancer_id, self.label, props
-
-    def get_edges(self):
-        with gzip.open(self.enhancer_gene_link_scores, 'rt') as f:
-            reader = csv.reader(f, delimiter=self.delimiter)
-            next(reader)    # Skip header
-            for line in reader:
-                enhancer_id = line[self.INDEX['enhancer']]
-                gene = self.handel_gene(line[self.INDEX['gene']])
-                uniprotkb = line[self.INDEX['gene']].split('|')[2].split('=')[1]
-
-                props = {}
+            if check_genomic_location(self.chr, self.start, self.end, chr, start, end):
                 if self.write_properties:
-                    props['uniprotkb'] = uniprotkb
+                    props['id'] = enhancer_id
+                    props['chr'] = chr
+                    props['start'] = start
+                    props['end'] = end
+                    props['data_source'] = data_source
 
                     if self.add_provenance:
                         props['source'] = self.source
                         props['source_url'] = self.source_url
                 
-                yield enhancer_id, gene, self.label, props
+                yield enhancer_region_id, self.label, props
+
+    def get_edges(self):
+        enhancer_id_map = {}
+        with gzip.open(self.enhancers_file, 'rt') as f:
+            reader = csv.reader(f, delimiter=self.delimiter)
+            for line in reader:
+                id = line[self.INDEX['id']]
+                chr = line[self.INDEX['chr']]
+                start = int(line[self.INDEX['start']])
+                end = int(line[self.INDEX['end']])
+                if check_genomic_location(self.chr, self.start, self.end, chr, start, end):
+                    region_id = build_regulatory_region_id(chr, start, end)
+                    enhancer_id_map[id] = region_id
+        
+        with gzip.open(self.enhancer_gene_link, 'rt') as f:
+            reader = csv.reader(f, delimiter=self.delimiter)
+            next(reader)    # Skip header
+            for line in reader:
+                id = line[self.INDEX['enhancer']]
+                if id not in enhancer_id_map:
+                    continue
+                enhancer_region_id = enhancer_id_map[id]
+                gene_hgnc_id = self.handel_gene(line[self.INDEX['gene']])
+                if gene_hgnc_id not in self.hgnc_ensembl_map:
+                    continue
+                gene = self.hgnc_ensembl_map[gene_hgnc_id]
+                tissue_id = line[self.INDEX['tissue']]
+                score = None
+                if self.INDEX['score'] < len(line):
+                    score = line[self.INDEX['score']]
+                if tissue_id not in self.tissue_ontology_map:
+                    continue
+
+                props = {}
+                if self.write_properties:
+                    props['biological_context'] = self.tissue_ontology_map[tissue_id][0]
+                    if score:
+                        props['score'] = score
+
+                    if self.add_provenance:
+                        props['source'] = self.source
+                        props['source_url'] = self.source_url
+                
+                yield enhancer_region_id, gene, self.label, props
             

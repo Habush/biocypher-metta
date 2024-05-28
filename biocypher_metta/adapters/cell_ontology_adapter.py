f710d2a4bf87d595d28b024e8c870bb50ec1e407
kedistkid <kedistkid723@gmail.com>
2024-05-28 17:25:04 +0300

    remove get_graph method from the cell ontology adapter

--- biocypher_metta/adapters/cell_ontology_adapter.py
+++ biocypher_metta/adapters/cell_ontology_adapter.py
@@ -1,13 +1,9 @@
 from biocypher_metta.adapters.ontologies_adapter import OntologyAdapter
-
 class CellOntologyAdapter(OntologyAdapter):
     def __init__(self, write_properties, add_provenance, type, label='cell', dry_run=False):
-        super().__init__(write_properties, add_provenance, type, label, dry_run)
+        super().__init__(write_properties, add_provenance, type, label, 'clo', dry_run)
         self.source = "Cell Ontology"
         self.source_url = "http://purl.obolibrary.org/obo/clo.owl"
         self.ONTOLOGIES = {
             'clo': 'http://purl.obolibrary.org/obo/clo.owl'
         }
-
-    def get_graph(self, ontology='clo'):
-        return super().get_graph(ontology)

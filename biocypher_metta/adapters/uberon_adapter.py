b7234acb56745950044f7b0e4b1638f7c317986a
kedistkid <kedistkid723@gmail.com>
2024-05-29 22:56:24 +0300

    remove get graph from uberon adapter and add ontology argument in the constructor

--- biocypher_metta/adapters/uberon_adapter.py
+++ biocypher_metta/adapters/uberon_adapter.py
@@ -1,13 +1,10 @@
 from biocypher_metta.adapters.ontologies_adapter import OntologyAdapter
 
 class UberonAdapter(OntologyAdapter):
-    def __init__(self, write_properties, add_provenance, type, label='ontology term', dry_run=False):
-        super().__init__(write_properties, add_provenance, type, label, dry_run)
+    def __init__(self, write_properties, add_provenance, ontology, type, label="ontology term", dry_run=False):
+        super().__init__(write_properties, add_provenance, ontology, type, label, dry_run)
         self.source = "UBERON"
         self.source_url = "http://purl.obolibrary.org/obo/uberon.owl"
         self.ONTOLOGIES = {
             'uberon': 'http://purl.obolibrary.org/obo/uberon.owl'
         }
-
-    def get_graph(self, ontology='uberon'):
-        return super().get_graph(ontology)

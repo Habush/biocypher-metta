be8264866a0731b54f1a8262d803e842e327c7cb
Dawit Melka <davebadeg@gmail.com>
2024-05-29 11:05:54 +0300

    Update biological context to ontlogy id

--- biocypher_metta/adapters/enhancer_atlas_adapter.py
+++ biocypher_metta/adapters/enhancer_atlas_adapter.py
@@ -1,6 +1,8 @@
-from collections import defaultdict
 import gzip
+import os
+import pickle
 from biocypher_metta.adapters import Adapter
+from biocypher_metta.adapters.helpers import build_regulatory_region_id, check_genomic_location
 
 # Example enhancer atlas input file:
 
@@ -17,19 +19,20 @@ from biocypher_metta.adapters import Adapter
 # chr1:955750-957580_ENSG00000225880$LINC00115$chr1$762902$-	1.512501
 # chr1:874840-876520_ENSG00000237330$RNF223$chr1$1009687$-	1.065467
 
-# enhancer_snps_file
-# chr1	2165300	2167010	11.464638265294	chr1	2166404	2166408	rs61776614
-# chr1	2165300	2167010	11.464638265294	chr1	2166807	2166811	rs76875252
-# chr1	7530490	7533550	16.9883353758835	chr1	7532288	7532292	rs1149336
-# chr1	8021850	8022240	13.3539912947246	chr1	8021971	8021975	rs35675666
 
 class EnhancerAtlasAdapter(Adapter):
     INDEX = {'chr': 0, 'coord_start': 1, 'coord_end': 2, 'snp': 7}
 
-    def __init__(self, filepath, enhancer_gene_filepath, enhancer_snps_filepath, write_properties, add_provenance, type='enhancer', label='enhancer'):
-        self.filepath = filepath
+    def __init__(self, enhancer_filepath, enhancer_gene_filepath, tissue_to_ontology_filepath, 
+                 write_properties, add_provenance, 
+                 type='enhancer', label='enhancer',
+                 chr=None, start=None, end=None):
+        self.enhancer_filepath = enhancer_filepath
         self.enhancer_gene_filepath = enhancer_gene_filepath
-        self.enhancer_snps_filepath = enhancer_snps_filepath
+        self.tissue_to_ontology_filepath = tissue_to_ontology_filepath
+        self.chr = chr
+        self.start = start
+        self.end = end
         self.label = label
         self.type = type
 
@@ -39,67 +42,58 @@ class EnhancerAtlasAdapter(Adapter):
 
         super(EnhancerAtlasAdapter, self).__init__(write_properties, add_provenance)
     
-    def get_enhancer_id(self, chr, start, end):
-        return chr+':'+start+'-'+end
-
+    def parse_enhancer_gene(self, info):
+        enhancer_info = info.split('_')[0]
+        chr = enhancer_info.split(':')[0]
+        start = int(enhancer_info.split(':')[1].split('-')[0]) + 1 #+1 since it is 0-based genomic coordinate
+        end = int(enhancer_info.split(':')[1].split('-')[1]) + 1
+        gene = info.split('_')[1].split('$')[0]
+        return chr, start, end, gene
+    
     def get_nodes(self):
-        enhancer_gene = defaultdict(list)
-        with gzip.open(self.enhancer_gene_filepath, 'rt') as f:
-            for line in f:
-                info = line.strip().split('_')
-                enhancer = info[0]
-                gene = info[1].split('$')[0]
-                enhancer_gene[enhancer].append(gene)
-        
-        enhancer_snps = defaultdict(list)
-        with gzip.open(self.enhancer_snps_filepath, 'rt') as f:
+        with gzip.open(self.enhancer_filepath, 'rt') as f:
             for line in f:
                 info = line.strip().split('\t')
                 chr = info[EnhancerAtlasAdapter.INDEX['chr']]
-                start = info[EnhancerAtlasAdapter.INDEX['coord_start']]
-                end = info[EnhancerAtlasAdapter.INDEX['coord_end']]
-                enhancer = self.get_enhancer_id(chr, start, end)
-                snp = info[EnhancerAtlasAdapter.INDEX['snp']]
-                enhancer_snps[enhancer].append(snp)
-        
-        with gzip.open(self.filepath, 'rt') as f:
-            for line in f:
-                info = line.strip().split('\t')
-                chr = info[EnhancerAtlasAdapter.INDEX['chr']]
-                start = info[EnhancerAtlasAdapter.INDEX['coord_start']]
-                end = info[EnhancerAtlasAdapter.INDEX['coord_end']]
-                enhancer = self.get_enhancer_id(chr, start, end)
-                genes = enhancer_gene.get(enhancer, [])
-                snps = enhancer_snps.get(enhancer, [])
+                start = int(info[EnhancerAtlasAdapter.INDEX['coord_start']]) + 1 #+1 since it is 0-based genomic coordinate
+                end = int(info[EnhancerAtlasAdapter.INDEX['coord_end']]) + 1
+                enhancer_region_id = build_regulatory_region_id(chr, start, end)
                 
-                props = {}
-                if self.write_properties:
-                    props['chr'] = chr
-                    props['start'] = start
-                    props['end'] = end
-                    if genes:
-                        props['genes'] = genes
-                    if snps:
-                        props['snps'] = snps
-                
-                    if self.add_provenance:
-                        props['source'] = self.source
-                        props['source_url'] = self.source_url
+                if check_genomic_location(self.chr, self.start, self.end, chr, start, end):
+                    props = {}
+                    if self.write_properties:
+                        props['chr'] = chr
+                        props['start'] = start
+                        props['end'] = end
+                    
+                        if self.add_provenance:
+                            props['source'] = self.source
+                            props['source_url'] = self.source_url
 
-                yield enhancer, self.label, props 
+                    yield enhancer_region_id, self.label, props 
 
     def get_edges(self):
-        with gzip.open(self.enhancer_gene_filepath, 'rt') as f:
-            for line in f:
-                info = line.strip().split('\t')
-                enhancer = info[0].split('_')[0]
-                gene = info[0].split('_')[1].split('$')[0]
-                score = info[1]
-                props = {}
-                if self.write_properties:
-                    props['score'] = score
-                    if self.add_provenance:
-                        props['source'] = self.source
-                        props['source_url'] = self.source_url
+        tissues = [f for f in os.listdir(self.enhancer_gene_filepath) if os.path.isfile(os.path.join(self.enhancer_gene_filepath, f))]
+        with open(self.tissue_to_ontology_filepath, 'rb') as f:
+            tissues_ontology_map = pickle.load(f)
+        
+        for tissue in tissues:
+            tissue_file_path = os.path.join(self.enhancer_gene_filepath, tissue)
+            biological_context = tissues_ontology_map.get(tissue.replace('_EP.txt', ''))
+            if biological_context:
+                with open(tissue_file_path, 'r') as f:
+                    for line in f:
+                        info = line.strip().split('\t')
+                        chr, start, end, gene = self.parse_enhancer_gene(line)
+                        if check_genomic_location(self.chr, self.start, self.end, chr, start, end):
+                            enhancer_region_id = build_regulatory_region_id(chr, start, end)
+                            score = float(info[1])
+                            props = {}
+                            if self.write_properties:
+                                props['biological_context'] = biological_context
+                                props['score'] = score
+                                if self.add_provenance:
+                                    props['source'] = self.source
+                                    props['source_url'] = self.source_url
 
-                yield enhancer, gene, self.label, props
+                            yield enhancer_region_id, gene, self.label, props

1e395f1f475aef814d800c231dd702887618250e
Abdulrahman S. Omar <hsamireh@gmail.com>
2024-06-10 13:23:47 +0300

    Fix bug with Prolog Writer

--- biocypher_metta/prolog_writer.py
+++ biocypher_metta/prolog_writer.py
@@ -8,11 +8,10 @@ import networkx as nx
 class PrologWriter:
 
     def __init__(self, schema_config, biocypher_config,
-                 output_dir, write_properties):
+                 output_dir):
         self.schema_config = schema_config
         self.biocypher_config = biocypher_config
         self.output_path = pathlib.Path(output_dir)
-        self.write_properties = write_properties
 
         if not os.path.exists(output_dir):
             self.output_path.mkdir()
@@ -86,29 +85,22 @@ class PrologWriter:
             label = label.split(".")[1]
         label = label.lower()
         id = id.lower()
-        def_out = f"{self.convert_input_labels(label)}({id})."
-        if self.write_properties:
-            return self.write_property(def_out, properties)
-        return [def_out]
+        def_out = f"{self.convert_input_labels(label)}({id})"
+        return self.write_property(def_out, properties)
 
     def write_edge(self, edge):
-        _id, source_id, target_id, label, properties = edge
+        source_id, target_id, label, properties = edge
         label = label.lower()
         source_id = source_id.lower()
         target_id = target_id.lower()
         source_type = self.edge_node_types[label]["source"]
         target_type = self.edge_node_types[label]["target"]
-        def_out = f"{label}({source_type}({source_id}), {target_type}({target_id}))."
-        if self.write_properties:
-            def_out_2 = f"{label}({source_type}({source_id}), {target_type}({target_id}))"
-            out_str = self.write_property(def_out_2, properties)
-            out_str.insert(0, def_out)
-            return out_str
-        return [def_out]
+        def_out = f"{label}({source_type}({source_id}), {target_type}({target_id}))"
+        return self.write_property(def_out, properties)
 
 
     def write_property(self, def_out, property):
-        out_str = []
+        out_str = [f"{def_out}."]
         for k, v in property.items():
             if k in self.excluded_properties or v is None or v == "": continue
             if isinstance(v, list):
